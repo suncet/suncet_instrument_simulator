b799fae0d6368d8f32e451242633c61242d3ec78
James Mason <jmason86@gmail.com>
2022-11-16 11:26:43 -0500

    Maybe got wavelength getter to work in cloud test

--- tests/test_instrument.py
+++ tests/test_instrument.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import urllib.request
 import ssl
 import numpy as np
@@ -11,13 +12,25 @@ tmp_file_urls = ["https://www.dropbox.com/s/bctrdr7de28m99o/B4C_Mo_Al_1-11000A.t
                  "https://www.dropbox.com/s/5tkfvphczs0a929/euv_sim_300_193A.fits?dl=1"] # dl=1 is important
 
 def test_instrument():
+    setup_environment()
     hardware = setup_instrument_hardware()
     run_mirror_coating_tests(hardware)
     delete_tmp_file()
 
 
-def setup_instrument_hardware():
+def setup_environment(): 
     download_test_data()
+    
+    if os.getenv('suncet_data') == None: 
+        os.environ['suncet_data'] = './'
+        path = Path('./mhd/dimmest/rendered_euv_maps')
+        path.mkdir(parents=True, exist_ok=True)
+        for url in tmp_file_urls: 
+            filename = url.rsplit('/', 1)[-1].rsplit('?', 1)[0]
+            Path('./' + filename).rename(path / filename)
+
+
+def setup_instrument_hardware():
     config_filename = os.getcwd() + '/suncet_instrument_simulator/config_files/config_default.ini'
     config = config_parser.Config(config_filename)
     hardware = instrument.Hardware(config)
