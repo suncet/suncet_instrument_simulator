708713c9a0036563d92d02ef16dcbee75f06de4e
James Mason <jmason86@gmail.com>
2023-09-15 16:05:14 -0600

    Download metadata

--- setup_minimum_required_folders_files.py
+++ setup_minimum_required_folders_files.py
@@ -12,7 +12,8 @@ tmp_file_urls = ["https://www.dropbox.com/s/bctrdr7de28m99o/B4C_Mo_Al_1-11000A.t
                  "https://www.dropbox.com/s/z86h2h7l8pgbhnl/filter_entrance_transmission.csv?dl=1",
                  "https://www.dropbox.com/s/muclu9kncl7xyff/filter_focal_plane_transmission.csv?dl=1",
                  "https://www.dropbox.com/s/tvqt8ybipa1bm8z/aia_V9_fullemiss.nc?dl=1", 
-                 "https://www.dropbox.com/s/k1qv0lujqmpvszn/si_qe_henke.csv?dl=1"] # dl=1 is important
+                 "https://www.dropbox.com/s/k1qv0lujqmpvszn/si_qe_henke.csv?dl=1",
+                 "https://www.dropbox.com/scl/fi/rbe7vm3sha9mbloek1iio/suncet_metadata_definition_v1.0.0.csv?rlkey=mswa2lvdrvbb9o1rer1z60p2x&dl=1"] # dl=1 is important
 
 def run():
     if os.getenv('suncet_data') == None:
@@ -34,6 +35,8 @@ def run():
     synthetic_path.mkdir(parents=True, exist_ok=True)
     synthetic_path = Path(os.getenv('suncet_data') + '/synthetic/level0_raw/binary')
     synthetic_path.mkdir(parents=True, exist_ok=True)
+    metadata_path = Path(os.getenv('suncet_data') + '/metadata')
+    metadata_path.mkdir(parents=True, exist_ok=True)
 
     ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -54,6 +57,8 @@ def run():
             filename = filter_path / filename
         elif filename.startswith('si_qe_'):
             filename = qe_path / filename
+        elif filename.startswith('suncet_metadata_'):
+            filename = metadata_path / filename
         with open(filename, "wb") as f:
             print('downloading file: {}'.format(filename))
             f.write(data)
