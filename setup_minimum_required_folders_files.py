ee148ced1fd8e01e1b602df4a82d60008e7e5918
James Mason <jmason86@gmail.com>
2023-09-15 13:44:38 -0600

    Download new rendered map

--- setup_minimum_required_folders_files.py
+++ setup_minimum_required_folders_files.py
@@ -7,7 +7,7 @@ import ssl
 
 tmp_file_urls = ["https://www.dropbox.com/s/bctrdr7de28m99o/B4C_Mo_Al_1-11000A.txt?dl=1", 
                  "https://www.dropbox.com/s/f51fep2nu1vr7ai/euv_sim_300_171A.fits?dl=1", 
-                 "https://www.dropbox.com/s/5tkfvphczs0a929/euv_sim_300_193A.fits?dl=1",
+                 "https://www.dropbox.com/scl/fi/kyyzrfla5su6831j0983e/SunCET_MapSeq_044.fits?rlkey=w6mfhov7pzmjljmmsvobqed4s&dl=1",
                  "https://www.dropbox.com/scl/fi/rbgyhy7lmtftge9m8gaoq/em_map_200.sav?rlkey=4fg96y3edldbja6qy9jhp9bt0&dl=1",
                  "https://www.dropbox.com/s/z86h2h7l8pgbhnl/filter_entrance_transmission.csv?dl=1",
                  "https://www.dropbox.com/s/muclu9kncl7xyff/filter_focal_plane_transmission.csv?dl=1",
@@ -44,7 +44,7 @@ def run():
         filename = get_filename_from_url(url)
         if filename.endswith('fullemiss.nc'):
             filename = emissivity_path / filename
-        elif filename.startswith('euv_sim_'):
+        elif filename.startswith('SunCET_MapSeq_'):
             filename = rendered_path / filename
         elif filename.startswith('em_map_'):
             filename = em_path / filename
